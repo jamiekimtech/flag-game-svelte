6d3254ca9917dbc473090c8e38c9282d5ecebf2f
Jamie Kim <jamiekimtech313@gmail.com>
2023-07-02 22:31:38 -0400

    Flip Animation

--- src/routes/flipcardgame/+page.svelte
+++ src/routes/flipcardgame/+page.svelte
@@ -1,71 +1,107 @@
 <script>
-	import { onMount } from 'svelte';
-	import axios from 'axios';
+	import { countryData } from './countryData.js';
+	import Flashcard from './Flashcard.svelte';
 
-	let flag = '';
-	let countryName = '';
-	let answerVisible = false;
+	let flashcardIndex = 0;
+	$: clue = countryData[flashcardIndex].flag;
+	$: answer = countryData[flashcardIndex].country;
 
-	async function getRandomFlag() {
-		try {
-			const response = await axios.get('https://restcountries.com/v3.1/region/asia');
-			const countries = response.data;
+	let showCardBack = false;
+	const toggleShowBack = () => (showCardBack = !showCardBack);
 
-			// Select a random country from the response data
-			const randomIndex = Math.floor(Math.random() * countries.length);
-			const randomCountry = countries[randomIndex];
-
-			// Assign the flag and country name from the random country
-			flag = randomCountry.flags.png;
-			countryName = randomCountry.name.common;
-			answerVisible = false;
-			console.log('WORKING!');
-		} catch (error) {
-			console.error('ERROR!');
+	const prevCard = () => {
+		showCardBack = false;
+		if (flashcardIndex === 0) {
+			flashcardIndex = countryData.length - 1;
+		} else {
+			flashcardIndex -= 1;
 		}
-	}
+	};
 
-	function revealAnswer() {
-		answerVisible = true;
-	}
-
-	function newGame() {
-		getRandomFlag();
-	}
-
-	// Fetch a random flag on component mount
-	onMount(getRandomFlag);
+	const nextCard = () => {
+		showCardBack = false;
+		if (flashcardIndex === countryData.length - 1) {
+			flashcardIndex = 0;
+		} else {
+			flashcardIndex += 1;
+		}
+	};
 </script>
 
 <main>
 	<h1>Flip the Card to Reveal Answer</h1>
-	<div class="flag-container">
-		{#if flag}
-			{#if answerVisible}
-				<img src={flag} alt="Flag" />
-				<h3>{countryName}</h3>
-			{:else}
-				<img src={flag} alt="Flag" />
-			{/if}
-		{:else}
-			<p>Loading...</p>
-		{/if}
+	<!-- FLASHCARD -->
+	<div class="flip-box">
+		<div class="flip-box-inner" class:flip-it={showCardBack}>
+			<Flashcard {clue} {answer} {showCardBack} />
+		</div>
 	</div>
-	<div class="answer-container" />
-	<button on:click={revealAnswer} disabled={answerVisible}>Reveal Answer</button>
-	<br />
-	<button on:click={newGame}>New Game</button>
+
+	<!-- BUTTONS -->
+	<div id="btn-cont">
+		<button class="arrow-btn" on:click={prevCard}>&#8592;</button>
+
+		<button on:click={toggleShowBack}>
+			{showCardBack ? 'Hide Answer' : 'Show Answer'}
+		</button>
+
+		<button class="arrow-btn" on:click={nextCard}>&#8594;</button>
+	</div>
 </main>
 
 <style>
-	button {
-		background-color: #154538;
-		border-width: 5px;
-		border-radius: 10px;
-		width: 500px;
-		margin: auto;
+	h1 {
+		margin-bottom: 20px;
 	}
 	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 0px;
+		height: 400px;
+	}
+
+	.flip-box {
+		background-color: transparent;
+		width: 400px;
+		height: 500px;
+		perspective: 1000px;
+		border-radius: 1000px;
+	}
+
+	/* This container is needed to position the front and back side */
+	.flip-box-inner {
+		position: relative;
+		width: 100%;
+		height: 100%;
 		text-align: center;
+		transition: transform 0.8s;
+		transform-style: preserve-3d;
+	}
+
+	/* Do an horizontal flip on button click */
+	.flip-it {
+		transform: rotateY(180deg);
+	}
+
+	#btn-cont {
+		width: 200px;
+		padding: 10px 0;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	button {
+		background-color: var(--primary);
+		padding: 10px 10px;
+		color: rgb(0, 0, 0);
+		cursor: pointer;
+		margin: 5px;
+		line-height: 0.7;
+		font-weight: 800;
+	}
+
+	button:active {
+		background-color: var(--primary-hover);
 	}
 </style>
